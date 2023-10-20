b7db33ac6d6be0993401bbe7f48a1664d28601dc
Duncan Champney <DChampney@WareTo.com>
2023-10-20 14:19:43 -0400

    Initial Commit

---
+++ RoundedCornerPolygonSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoundedCornerPolygonSwiftUI
+//
+//  Created by Duncan Champney on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoundedCornerPolygonSwiftUI/RoundedCornerPolygonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedCornerPolygonSwiftUIApp.swift
+//  RoundedCornerPolygonSwiftUI
+//
+//  Created by Duncan Champney on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct RoundedCornerPolygonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
